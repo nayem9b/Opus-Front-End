7b28d456cec85f76decc49365fb8a133fe87242b
nayem9b <razwanniam1@gmail.com>
2022-12-22 23:32:21 +0600

    navbar added

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "../Footer/Footer";
+import Navbar from "../Navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h1>This is home Component</h1>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Home;
