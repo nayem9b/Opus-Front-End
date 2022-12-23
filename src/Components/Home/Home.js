6fb66eafbacbbbe038c8119ba9b47e9dd637aa3a
nayem9b <razwanniam1@gmail.com>
2022-12-23 07:09:19 +0600

    stats complete

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,13 +2,14 @@ import React from "react";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import Navbar from "../Navbar/Navbar";
+import Stats from "../Stats/Stats";
 
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Header></Header>
-      <h1>This is home Component</h1>
+      <Stats></Stats>
       <Footer></Footer>
     </div>
   );
