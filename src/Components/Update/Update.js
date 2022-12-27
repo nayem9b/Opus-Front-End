b7e1d5d79b85ca03a6099f47284e2c72a7ee5ed5
nayem9b <razwanniam1@gmail.com>
2022-12-27 12:58:28 +0600

    update page modified

--- src/Components/Update/Update.js
+++ src/Components/Update/Update.js
@@ -3,9 +3,38 @@ import React from "react";
 const Update = () => {
   return (
     <div>
-      <h1 className='text-lg'>This is update page</h1>
-      <p className='text-3xl'>This is update page</p>
-      <p>hola</p>
+      <section class=''>
+        <div class='max-w-3xl px-6 py-16 '>
+          <h1 class='text-3xl font-semibold '>Want to update the header?</h1>
+
+          <div class='flex flex-col mt-8 space-y-3 sm:space-y-0 sm:flex-row sm:-mx-2'>
+            <input
+              id='email'
+              type='text'
+              class='pr-20 py-2  border rounded-md sm:mx-2 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40'
+              placeholder='Text'
+            />
+
+            <button class='px-4 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-[#1355FF] rounded-md sm:mx-2 hover:bg-[#033dd1] focus:outline-none focus:bg-blue-600'>
+              Update
+            </button>
+          </div>
+        </div>
+        <div class='max-w-3xl px-6 py-16 '>
+          <h1 class='text-3xl font-semibold '>Want to update image?</h1>
+
+          <div class='flex flex-col mt-8 space-y-3 sm:space-y-0 sm:flex-row sm:-mx-2'>
+            <input
+              type='file'
+              className='file-input file-input-bordered file-input-primary w-full max-w-xs'
+            />
+
+            <button class='px-4 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-[#1355FF] rounded-md sm:mx-2 hover:bg-[#033dd1] focus:outline-none focus:bg-blue-600'>
+              Update
+            </button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
